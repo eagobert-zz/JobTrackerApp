175df7db284189daa36d815823bbea2ff122f51e
eagobert <eagobert@hotmail.com>
2018-03-01 16:02:21 -0600

    Initial Commit

---
+++ JobTrackerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobTrackerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
