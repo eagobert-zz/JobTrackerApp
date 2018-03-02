a71c8b35ef6acadb3cb6a1db10ff42f9ca27b637
eagobert <eagobert@hotmail.com>
2018-03-02 15:16:00 -0600

    scaffolding and database migration complete

---
+++ JobTrackerApp/Controllers/Job_ContactController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JobTrackerApp.Data;
+using JobTrackerApp.Models;
+
+namespace JobTrackerApp.Controllers
+{
+    public class Job_ContactController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Job_ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Job_Contact
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Job_Contact.Include(j => j.Contact).Include(j => j.Job);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Job_Contact/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job_Contact = await _context.Job_Contact
+                .Include(j => j.Contact)
+                .Include(j => j.Job)
+                .SingleOrDefaultAsync(m => m.Job_ContactId == id);
+            if (job_Contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(job_Contact);
+        }
+
+        // GET: Job_Contact/Create
+        public IActionResult Create()
+        {
+            ViewData["ContactId"] = new SelectList(_context.Contact, "ContactId", "POC_firstName");
+            ViewData["JobId"] = new SelectList(_context.Job, "JobId", "Job_Post_Url");
+            return View();
+        }
+
+        // POST: Job_Contact/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Job_ContactId,JobId,ContactId")] Job_Contact job_Contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(job_Contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContactId"] = new SelectList(_context.Contact, "ContactId", "POC_firstName", job_Contact.ContactId);
+            ViewData["JobId"] = new SelectList(_context.Job, "JobId", "Job_Post_Url", job_Contact.JobId);
+            return View(job_Contact);
+        }
+
+        // GET: Job_Contact/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job_Contact = await _context.Job_Contact.SingleOrDefaultAsync(m => m.Job_ContactId == id);
+            if (job_Contact == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContactId"] = new SelectList(_context.Contact, "ContactId", "POC_firstName", job_Contact.ContactId);
+            ViewData["JobId"] = new SelectList(_context.Job, "JobId", "Job_Post_Url", job_Contact.JobId);
+            return View(job_Contact);
+        }
+
+        // POST: Job_Contact/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Job_ContactId,JobId,ContactId")] Job_Contact job_Contact)
+        {
+            if (id != job_Contact.Job_ContactId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(job_Contact);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Job_ContactExists(job_Contact.Job_ContactId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContactId"] = new SelectList(_context.Contact, "ContactId", "POC_firstName", job_Contact.ContactId);
+            ViewData["JobId"] = new SelectList(_context.Job, "JobId", "Job_Post_Url", job_Contact.JobId);
+            return View(job_Contact);
+        }
+
+        // GET: Job_Contact/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job_Contact = await _context.Job_Contact
+                .Include(j => j.Contact)
+                .Include(j => j.Job)
+                .SingleOrDefaultAsync(m => m.Job_ContactId == id);
+            if (job_Contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(job_Contact);
+        }
+
+        // POST: Job_Contact/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var job_Contact = await _context.Job_Contact.SingleOrDefaultAsync(m => m.Job_ContactId == id);
+            _context.Job_Contact.Remove(job_Contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool Job_ContactExists(int id)
+        {
+            return _context.Job_Contact.Any(e => e.Job_ContactId == id);
+        }
+    }
+}
---
+++ JobTrackerApp/Data/Migrations/20180302210745_Initial.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace JobTrackerApp.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(nullable: false),
+                    CompanyId1 = table.Column<int>(nullable: true),
+                    CompanyName = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.CompanyId);
+                    table.ForeignKey(
+                        name: "FK_Company_Company_CompanyId1",
+                        column: x => x.CompanyId1,
+                        principalTable: "Company",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CompanyId = table.Column<int>(nullable: false),
+                    ContactId1 = table.Column<int>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    POC_firstName = table.Column<string>(nullable: false),
+                    POC_lastName = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.ContactId);
+                    table.ForeignKey(
+                        name: "FK_Contact_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contact_Contact_ContactId1",
+                        column: x => x.ContactId1,
+                        principalTable: "Contact",
+                        principalColumn: "ContactId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    JobId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CompanyId = table.Column<int>(nullable: false),
+                    JobId1 = table.Column<int>(nullable: true),
+                    Job_Post_Url = table.Column<string>(nullable: false),
+                    Post_Date = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.JobId);
+                    table.ForeignKey(
+                        name: "FK_Job_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Job_Job_JobId1",
+                        column: x => x.JobId1,
+                        principalTable: "Job",
+                        principalColumn: "JobId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Job_Contact",
+                columns: table => new
+                {
+                    Job_ContactId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ContactId = table.Column<int>(nullable: false),
+                    JobId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job_Contact", x => x.Job_ContactId);
+                    table.ForeignKey(
+                        name: "FK_Job_Contact_Contact_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contact",
+                        principalColumn: "ContactId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Job_Contact_Job_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Job",
+                        principalColumn: "JobId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_CompanyId1",
+                table: "Company",
+                column: "CompanyId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contact_CompanyId",
+                table: "Contact",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contact_ContactId1",
+                table: "Contact",
+                column: "ContactId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_CompanyId",
+                table: "Job",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_JobId1",
+                table: "Job",
+                column: "JobId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_Contact_ContactId",
+                table: "Job_Contact",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_Contact_JobId",
+                table: "Job_Contact",
+                column: "JobId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Job_Contact");
+
+            migrationBuilder.DropTable(
+                name: "Contact");
+
+            migrationBuilder.DropTable(
+                name: "Job");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
